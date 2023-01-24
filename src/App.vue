9d3d293ecaa9bb3092178b468f29c23f6073a89c
Tony-tuf15 <a.gryhoryev@gmail.com>
2023-01-24 20:13:19 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
+<script setup>
+
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="bg-green-800">
+    <h1 class="text-red-100">hi man</h1>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+header {
+  line-height: 1.5;
 }
 
-nav {
-  padding: 30px;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
